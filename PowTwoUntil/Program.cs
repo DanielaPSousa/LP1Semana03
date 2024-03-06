4419b57cc3127c0958d440fb1a62bd3d4498d82c
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-06 21:09:59 +0000

    Invoke the 2× method from Main

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Power2Until15();
+        }
+
+        private static void Power2Until15()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
