07ecb1f85c7a9694de159fd76ae72693f17ac66c
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-06 20:54:39 +0000

    Add float

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write 6 numbers:");
+
+            float arg1 = float.Parse(args[0]);
+            float arg2 = float.Parse(args[1]);
+            float arg3 = float.Parse(args[2]);
+            float arg4 = float.Parse(args[3]);
+            float arg5 = float.Parse(args[4]);
+            float arg6 = float.Parse(args[5]);
+
+            float[,] A = { { arg1, arg2 }, { arg3, arg4 } };
+            float[] b = { arg5, arg6 };
+
+            float[] result = MultiplyMatrixByVector(A, b);
+
+            Console.WriteLine($"{result[0]}");
+            Console.WriteLine($"{result[1]}");
+        }
+
+    }
+}
+
+        }
+    }
+}
