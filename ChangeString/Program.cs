63e57ea12d319b9e3d0ed7427ea3a9b8fecb6f02
Daniela Sousa <danielasousa.info@gmail.com>
2024-03-06 20:17:13 +0000

    Asking user for a string and character

---
+++ ChangeString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere uma string:")
+            string s= Console.ReadLine();
+
+            Console.Write("Insere um caráter:")
+            char crt= Console.ReadKey().KeyChar;
+        }
+    }
+}
